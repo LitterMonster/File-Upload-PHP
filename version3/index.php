f346d5e3c8ebf11eeaccd8f81d827acf97ffc8f0
zhangtao <sspu000000@163.com>
2016-04-16 21:37:23 +0800

    Support to delete files. the source code is in version3

---
+++ version3/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>管理中心</title>
+<meta charset="utf-8"/>
+</head>
+<body>
+<center>
+<a href="upload.php">  <font size="6" color="green">文件上传</font></a><br/>
+<a href="delete.php">  <font size="6" color="green">文件删除</font></a><br/>
+</center>
+</body>
+</html>
