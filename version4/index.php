cc456969fef461eb7e724fa9978eba8b06ed1b42
zhangtao <sspu000000@163.com>
2016-04-16 22:27:48 +0800

    Support to add new directory and the source code is in version4

---
+++ version4/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>管理中心</title>
+<meta charset="utf-8"/>
+</head>
+<body>
+<center>
+<a href="http://file.littermonster.net"><font size="6" color="green">文件阅览</font></a><br/>
+<a href="upload.php"><font size="6" color="green">文件上传</font></a><br/>
+<a href="delete.php"><font size="6" color="green">文件删除</font></a><br/>
+<a href="adddir.php"><font size="6" color="green">添加目录</font></a><br/>
+</center>
+</body>
+</html>
